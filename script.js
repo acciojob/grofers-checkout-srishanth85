b1fbd2517e71e92a69622cbef164fd3d67cd6225
srishanth85 <srishanth8085@gmail.com>
2025-10-26 08:34:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
+getSumBtn.textContent="Get Total Price";
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 	const prices = document.querySelectorAll(".prices");
+	let total=0;
 
-	prices.forEach(price=>{total+=Number(price.textContent)})
+	prices.forEach(price=>{
+		total+=Number(price.textContent);
+	});
 
 	const newrow = document.createElement("tr");
 	const newcell = document.createElement("td");
@@ -13,7 +16,10 @@ const getSum = () => {
 	newcell.colSpan = 2;
 	newcell.textContent=`Total Price : ${total}`;
 
-	newrow.appendChild(newrow);
+	newrow.appendChild(newcell);
+
+	const table= document.querySelector("table")
+	table.appendChild(newrow)
 	
 //Add your code here
   
