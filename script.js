3d7a13c66a7b5e0ac9433ae5c08535ac9b223afb
srishanth85 <srishanth8085@gmail.com>
2025-10-26 08:24:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
+	const prices = document.querySelectorAll(".prices");
+
+	prices.forEach(price=>{total+=Number(price.textContent)})
+
+	const newrow = document.createElement("tr");
+	const newcell = document.createElement("td");
+
+	newcell.colSpan = 2;
+	newcell.textContent=`Total Price : ${total}`;
+
+	newrow.appendChild(newrow);
+	
 //Add your code here
   
 };
