9606ee58fb84642d003ee12b9a05ca2c1ff89793
srishanth85 <srishanth8085@gmail.com>
2025-10-27 14:00:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-
 const getSumBtn = document.createElement("button");
 getSumBtn.textContent = "Get Total Price";
-document.body.appendChild(getSumBtn);
+document.body.appendChild(getSumBtn)
 
-const getSum = () => {
-  
-  const prices = document.querySelectorAll(".price");
-  let total = 0;
+const getSum = ()=>{
+	const prices = document.querySelectorAll('.price');
+	
+	let total = 0;
+	prices.forEach((p)=>{
+		total +=parseFloat(p.textContent)
+	})
+	const table = document.querySelector('table');
+	const newRow = document.createElement("tr");
+	const totalcell = document.createElement("td");
+	const levelcell = document.createElement("td");
+	
+	totalcell.textContent = total;
+	levelcell.textContent ='Total Price :';
 
- 
-  prices.forEach(price => {
-    total += Number(price.textContent);
-  });
-
-  
-  const newRow = document.createElement("tr");
-  const newCell = document.createElement("td");
-
-  newCell.colSpan = 2;
-  newCell.textContent = `Total Price: ${total}`;
-  newCell.style.fontWeight = "bold";
-
-  newRow.appendChild(newCell);
-
- 
-  const table = document.querySelector("table");
-  table.appendChild(newRow);
-};
-
-
-getSumBtn.addEventListener("click", getSum);
+	newRow.append(levelcell,totalcell);
+	table.appendChild(newRow);
+	
+}
+getSumBtn.addEventListener("click",getSum)
